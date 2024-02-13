1fcec58bcd90b0077fa3aa05639a6271dbdebf7a
Callum Hobbis <callum.hobbis@gmail.com>
2024-02-13 15:56:13 +1300

    feat: add function to play multiple rounds

--- rps.js
+++ rps.js
@@ -1,21 +1,49 @@
+var CHOICES = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
-  return choices[Math.floor(Math.random() * choices.length)];
+  return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection =
-    playerSelection[0].toUpperCase() +
-    playerSelection.substring(1).toLowerCase();
-
   const playerWins = ["PaperRock", "ScissorsPaper", "RockScissors"];
   const matchup = playerSelection + computerSelection;
-
-  if (playerWins.includes(matchup)) {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === computerSelection) {
-    return `You Draw! Both played ${playerSelection}`;
+  if (playerSelection === computerSelection) {
+    return "draw";
+  } else if (playerWins.includes(matchup)) {
+    return "win";
   } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    return "lose";
+  }
+}
+
+function playGame(rounds = 5) {
+  let score = 0;
+  for (let i = 1; i <= rounds; i++) {
+    let playerSelection;
+    while (!CHOICES.includes(playerSelection)) {
+      playerSelection = prompt("Enter move: ");
+      playerSelection =
+        playerSelection[0].toUpperCase() +
+        playerSelection.substring(1).toLowerCase();
+    }
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    if (result === "win") {
+      console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+      score += 1;
+    } else if (result == "draw") {
+      console.log(`You Draw! Both played ${playerSelection}`);
+    } else {
+      console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+      score -= 1;
+    }
+  }
+  console.log("---- GAME OVER ----");
+  if (score > 0) {
+    console.log("You beat the computer!");
+  } else if (score == 0) {
+    console.log("You tied with the computer!");
+  } else {
+    console.log("You lost to the computer!");
   }
 }
