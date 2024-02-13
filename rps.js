0316d22c6b0b987f86baaa4623d24dc3e6d14bca
Callum Hobbis <callum.hobbis@gmail.com>
2024-02-13 15:36:56 +1300

    feat: add function to play a round

--- rps.js
+++ rps.js
@@ -2,3 +2,20 @@ function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection =
+    playerSelection[0].toUpperCase() +
+    playerSelection.substring(1).toLowerCase();
+
+  const playerWins = ["PaperRock", "ScissorsPaper", "RockScissors"];
+  const matchup = playerSelection + computerSelection;
+
+  if (playerWins.includes(matchup)) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    return `You Draw! Both played ${playerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
